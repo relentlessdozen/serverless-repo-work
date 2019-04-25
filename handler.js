992d3e20ecc684ec3b564cffc7dcfdb29158406d
Rick James <patrickjamesjr@protonmail.com>
2019-04-25 07:25:46 -0400

    Added handler.js and serverless.yml files to begin the process of deploying the first function in this repo

---
+++ handler.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports.greet = (event, context, callback) => {
+  console.log(JSON.stringify(event));
+  callback(null, { message: "Hybrid Cloud Club intiation function!" });
+};
